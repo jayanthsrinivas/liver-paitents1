993aac595714c0dbcea60fbf2ae6c34432242b8d
jayanthsrinivas <31428603+jayanthsrinivas@users.noreply.github.com>
2019-08-27 16:44:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    z=[]
+    int_features=[]
+    for x in request.form.values():
+        z.append(x)
+    a=[z[0],z[1],z[2],z[3],z[4]]
+    b=[z[5],z[6],z[7],z[8]]
+    for c in request.form.values():
+        if c in a:
+            int_features.append(int(c))
+        if c in b:
+            int_features.append(float(c))
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    if prediction==[1]:
+        return render_template('index.html', prediction_text='patient is having liver disease')
+    if prediction==[0]:
+        return render_template('index.html', prediction_text='patient is not having liver disease')
+        
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
