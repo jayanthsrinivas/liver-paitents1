8959c352f7964ab4186a1e6adba6c45ae2dfdb8d
jayanthsrinivas <31428603+jayanthsrinivas@users.noreply.github.com>
2019-08-27 15:05:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,15 @@ def predict():
         z.append(x)
     a=[z[0],x[1],z[2],z[3],z[4]]
     b=[z[5],z[6],z[7],z[8]]
-    if c in a:
-        int_features.append(int(c))
-    if c in b:
-        int_features.append(float(c))
+    for x in request.form.values():
+        z.append(x)
+    a=[z[0],x[1],z[2],z[3],z[4]]
+    b=[z[5],z[6],z[7],z[8]]
+    for c in request.form.values():
+        if c in a:
+            int_features.append(int(c))
+        if c in b:
+            int_features.append(float(c))
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
